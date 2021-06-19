1ad6ed71dd82eb0afc46178172d610b47f6ea76b
pokharelsrj <pokharelsrj@gmail.com>
2021-06-20 00:09:01 +0545

    add store to user context

--- src/components/Context/UserContext.js
+++ src/components/Context/UserContext.js
@@ -1,3 +1,26 @@
-import { createContext } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 export const UserContext = createContext({});
+
+export const Store = (props) => {
+  const [user, setUserState] = useState({});
+  const initialState = JSON.parse(localStorage.getItem("user"));
+  useEffect(() => {
+    setUserState(initialState);
+  }, [initialState]);
+
+  function setUser(user) {
+    setUserState(user);
+    localStorage.setItem("user", JSON.stringify(user));
+  }
+
+  function resetUser() {
+    setUser(null);
+    localStorage.removeItem("user");
+  }
+  return (
+    <UserContext.Provider value={{ user, setUser, resetUser }}>
+      {props.children}
+    </UserContext.Provider>
+  );
+};
