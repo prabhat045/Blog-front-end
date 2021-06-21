4a5773cfb88f6644f651882b7c46b8be0ab180b2
prabhat045 <54550989+prabhat045@users.noreply.github.com>
2021-06-21 16:55:35 +0545

    getarticlesbackend

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -4,29 +4,30 @@ import "./article.css";
 const Articles = (props) => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [body, setBody] = useState("");
+  const [mainbody, setBody] = useState("");
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/article", {
+    fetch("http://localhost:5001/articles/getarticles", {
       method: "get",
       headers: { "Content-type": "application/json" },
     }).then((response) =>
       response.json().then((data) => {
         setData([data]);
+        console.log(data);
       })
     );
-  }, [data]);
+  }, []);
 
   return (
-    <div className="row">
+    <div className='row'>
       {data.map((data) => {
         return (
-          <div className="column" key={data.id}>
-            <div className="card">
-              <h3 className="title">{data.title} </h3>
-              <p className="description">{data.description}</p>
-              <p className="body">{data.body}</p>
+          <div className='column' key={data.id}>
+            <div className='card'>
+              <h3 className='title'>{data.title} </h3>
+              <p className='description'>{data.description}</p>
+              <p className='body'>{data.mainbody}</p>
             </div>
           </div>
         );
